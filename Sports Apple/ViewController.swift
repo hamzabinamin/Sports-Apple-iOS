94f33a660e818247e9789810607a9c589d405224
Hamza Amin <hamzaamin@192.168.8.100>
2018-05-04 12:56:06 +0500

    Initial Commit

---
+++ Sports Apple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sports Apple
+//
+//  Created by Hamza Amin on 5/4/18.
+//  Copyright © 2018 Hamza Amin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
